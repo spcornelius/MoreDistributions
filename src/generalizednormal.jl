1260c4a0e11aeb7b05bd4d47bfca0421e8f7b348
Sean P. Cornelius <spcornelius@gmail.com>
2022-03-03 12:17:26 -0500

    remove Manifest from VC; move @check_args to external constructor

--- src/generalizednormal.jl
+++ src/generalizednormal.jl
@@ -8,14 +8,16 @@ struct GeneralizedNormal{T<:Real} <: ContinuousUnivariateDistribution
     β::T
 
     function GeneralizedNormal{T}(μ, α, β) where {T}
-        @check_args(GeneralizedNormal, α > zero(T))
-        @check_args(GeneralizedNormal, β > zero(T))
         new{T}(μ, α, β)
     end
 end
 
 #### Outer constructors
-GeneralizedNormal(μ::T, α::T, β::T) where {T<:Real} = GeneralizedNormal{T}(μ, α, β)
+function GeneralizedNormal(μ::T, α::T, β::T; check_args::Bool=true) where {T<:Real}
+    @check_args GeneralizedNormal (α, α > zero(α)) (β, β > zero(β))
+    return GeneralizedNormal{T}(μ, α, β)
+end
+
 GeneralizedNormal(μ::Real, α::Real, β::Real) = GeneralizedNormal(promote(μ, α, β)...)
 GeneralizedNormal(μ::Integer, α::Integer, β::Integer) = GeneralizedNormal(float(μ), float(α), float(β))
 
