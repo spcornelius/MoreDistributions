f83aac0b8b2681562df7474f3be1366b56dadb9e
Sean P. Cornelius <spcornelius@gmail.com>
2022-08-03 13:43:39 -0400

    fix wrong import

--- src/MoreDistributions.jl
+++ src/MoreDistributions.jl
@@ -1,7 +1,7 @@
 module MoreDistributions
 
 using Distributions, LinearAlgebra, LazyArrays, SpecialFunctions, StatsBase,
-      Random, NLsolve, UnPack, HypergeometricFunctions
+      Random, NLopt, UnPack, HypergeometricFunctions
 using Distributions: @quantile_newton, quantile_newton, quantile_bisect
 
 # functions to extend
